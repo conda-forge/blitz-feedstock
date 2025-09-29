9b32ab6ef2c147fb5fdc8fcdc275230b445827a1
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 11:28:34 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -18,8 +19,15 @@ jq 'del(.devDependencies)' package.json.bak > package.json
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/blitz << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/blitz/bin/blitz "\$@"
+EOF
+chmod +x ${PREFIX}/bin/blitz
+
 tee ${PREFIX}/bin/blitz.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\blitz %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\blitz\bin\blitz %*
 EOF
 
 # Delete vendored esbuild so package is noarch
